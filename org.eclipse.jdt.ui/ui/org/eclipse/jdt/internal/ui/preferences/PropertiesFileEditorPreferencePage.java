c47038618ac344df4a7fe2b342a0484420bb2ff4
Zsombor Gegesy <gzsombor@gmail.com>
2021-08-14 23:36:21 +0200

    Simplify OverlayPreferenceStore a bit
    Change-Id: Idbb324ff24f6647fa0934ea529ccd722d7aa2a15

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/PropertiesFileEditorPreferencePage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/PropertiesFileEditorPreferencePage.java
@@ -360,7 +360,7 @@ public class PropertiesFileEditorPreferencePage extends PreferencePage implement
 
 	}
 
-	private OverlayPreferenceStore.OverlayKey[] createOverlayStoreKeys() {
+	private List<OverlayPreferenceStore.OverlayKey> createOverlayStoreKeys() {
 
 		ArrayList<OverlayKey> overlayKeys= new ArrayList<>();
 
@@ -373,9 +373,7 @@ public class PropertiesFileEditorPreferencePage extends PreferencePage implement
 			overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.BOOLEAN, colorKey + UNDERLINE));
 		}
 
-		OverlayPreferenceStore.OverlayKey[] keys= new OverlayPreferenceStore.OverlayKey[overlayKeys.size()];
-		overlayKeys.toArray(keys);
-		return keys;
+		return overlayKeys;
 	}
 
 	/*
